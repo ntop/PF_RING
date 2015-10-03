a1180dc6b098f99285b3d11cf3d13445d319d1a9
cardigliano <cardigliano@ntop.org>
2015-10-03 19:04:07 +0200

    new e1000e-zc driver v.3.2.4.2 (testing)

---
+++ drivers/ZC/intel/e1000e/e1000e-3.2.4.2-zc/src/load_driver.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+FAMILY=e1000e
+
+#service udev start
+
+# Remove old modules (if loaded)
+rmmod e1000e
+rmmod pf_ring
+
+HUGEPAGES=1024
+if [ `cat /proc/mounts | grep hugetlbfs | wc -l` -eq 0 ]; then
+	sync && echo 3 > /proc/sys/vm/drop_caches
+	echo $HUGEPAGES > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+	mkdir /mnt/huge
+	mount -t hugetlbfs nodev /mnt/huge
+fi
+AVAILHUGEPAGES=$(grep HugePages_Total /sys/devices/system/node/node0/meminfo | cut -d ':' -f 2|sed 's/ //g')
+if [ $AVAILHUGEPAGES -ne $HUGEPAGES ]; then 
+	printf "Warning: %s hugepages available, %s requested\n" "$AVAILHUGEPAGES" "$HUGEPAGES"
+fi
+
+# We assume that you have compiled PF_RING
+insmod ../../../../../../kernel/pf_ring.ko
+
+insmod ./e1000e.ko
+
+sleep 1
+
+killall irqbalance 
+
+INTERFACES=$(cat /proc/net/dev|grep ':'|grep -v 'lo'|grep -v 'sit'|awk -F":" '{print $1}'|tr -d ' ')
+for IF in $INTERFACES ; do
+	TOCONFIG=$(ethtool -i $IF|grep $FAMILY|wc -l)
+        if [ "$TOCONFIG" -eq 1 ]; then
+		printf "Configuring %s\n" "$IF"
+		ifconfig $IF up
+
+		# Disabling receive offloads
+		ethtool -K $IF tso off
+		ethtool -K $IF gso off
+		ethtool -K $IF gro off
+		sleep 1
+	fi
+done
