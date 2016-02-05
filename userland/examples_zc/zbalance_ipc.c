44b1eb040bacc75bf7e52e54a8730f4174144236
root <root@2WR38Y1.(none)>
2016-02-05 16:48:31 +0000

    correct placement of colon to force argument

--- userland/examples_zc/zbalance_ipc.c
+++ userland/examples_zc/zbalance_ipc.c
@@ -405,7 +405,7 @@ int main(int argc, char* argv[]) {
     opt_argv = argv;
   }
 
-  while ((c = getopt(opt_argc, opt_argv,"ab:c:dg:hi:m:n:pQ:q:N:P:R:S:z:u")) != '?') {
+  while ((c = getopt(opt_argc, opt_argv,"ab:c:dg:hi:m:n:pQ:q:N:P:R:S:zu:")) != '?') {
     if ((c == 255) || (c == -1)) break;
 
     switch (c) {
@@ -506,7 +506,7 @@ int main(int argc, char* argv[]) {
 
   if (daemon_mode)
     daemonize();
-
+    printf("%u\n",(num_real_devices * MAX_CARD_SLOTS) + (num_in_queues * (queue_len + IN_POOL_SIZE)) + (num_consumer_queues * (queue_len + pool_size)) + PREFETCH_BUFFERS + num_additional_buffers);
   zc = pfring_zc_create_cluster(
     cluster_id, 
     max_packet_len(devices[0]),
