e9fb3f37c9a5215ae270fc57c83d00294ac83181
Alfredo Cardigliano <cardigliano@ntop.org>
2016-06-07 18:46:27 +0200

    Moved dev to v.6.5.0

--- kernel/linux/pf_ring.h
+++ kernel/linux/pf_ring.h
@@ -41,8 +41,8 @@
 #define pfring_ptr ax25_ptr
 
 /* Versioning */
-#define RING_VERSION                "6.3.0"
-#define RING_VERSION_NUM           0x060300
+#define RING_VERSION                "6.5.0"
+#define RING_VERSION_NUM           0x060500
 
 /* Set */
 #define SO_ADD_TO_CLUSTER                 99
