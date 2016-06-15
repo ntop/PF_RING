78aa26a19bc57da003faca805e4900c9944a49ae
cardigliano <cardigliano@ntop.org>
2016-06-15 15:32:18 +0200

    moved stable to v.6.4.1

--- kernel/linux/pf_ring.h
+++ kernel/linux/pf_ring.h
@@ -41,8 +41,8 @@
 #define pfring_ptr ax25_ptr
 
 /* Versioning */
-#define RING_VERSION                "6.4.0"
-#define RING_VERSION_NUM           0x060400
+#define RING_VERSION                "6.4.1"
+#define RING_VERSION_NUM           0x060401
 
 /* Set */
 #define SO_ADD_TO_CLUSTER                 99
