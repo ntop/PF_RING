0192d5e22b80b8cc3f3b16922d5cef7803286ca1
cardigliano <cardigliano@ntop.org>
2016-09-01 19:52:04 +0200

    increased max num cluster sockets 32 -> 64

--- kernel/linux/pf_ring.h
+++ kernel/linux/pf_ring.h
@@ -870,7 +870,7 @@ struct send_msg_to_plugin_info {
 #endif
 #endif
 
-#define CLUSTER_LEN       32
+#define CLUSTER_LEN       64
 
 /*
  * A ring cluster is used group together rings used by various applications
