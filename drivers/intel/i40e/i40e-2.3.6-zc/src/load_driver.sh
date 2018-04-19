765a88311229c6c8222fe3e49eb89d070a23c381
Alfredo Cardigliano <cardigliano@ntop.org>
2018-04-19 16:25:54 +0200

    hugepages fix

--- drivers/intel/i40e/i40e-2.3.6-zc/src/load_driver.sh
+++ drivers/intel/i40e/i40e-2.3.6-zc/src/load_driver.sh
@@ -8,18 +8,6 @@ FAMILY=i40e
 rmmod i40e
 rmmod pf_ring
 
-HUGEPAGES=1024
-if [ `cat /proc/mounts | grep hugetlbfs | wc -l` -eq 0 ]; then
-	sync && echo 3 > /proc/sys/vm/drop_caches
-	echo $HUGEPAGES > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
-	mkdir /mnt/huge
-	mount -t hugetlbfs nodev /mnt/huge
-fi
-AVAILHUGEPAGES=$(grep HugePages_Total /sys/devices/system/node/node0/meminfo | cut -d ':' -f 2|sed 's/ //g')
-if [ $AVAILHUGEPAGES -ne $HUGEPAGES ]; then 
-	printf "Warning: %s hugepages available, %s requested\n" "$AVAILHUGEPAGES" "$HUGEPAGES"
-fi
-
 # We assume that you have compiled PF_RING
 insmod ../../../../../kernel/pf_ring.ko
 
@@ -76,4 +64,19 @@ for IF in $INTERFACES ; do
 		bash ../scripts/set_irq_affinity $IF
 	fi
 done
+
+HUGEPAGES_NUM=1024
+HUGEPAGES_PATH=/dev/hugepages
+sync && echo 3 > /proc/sys/vm/drop_caches
+echo $HUGEPAGES_NUM > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+if [ `cat /proc/mounts | grep hugetlbfs | grep $HUGEPAGES_PATH | wc -l` -eq 0 ]; then
+	if [ ! -d $HUGEPAGES_PATH ]; then
+		mkdir $HUGEPAGES_PATH
+	fi
+	mount -t hugetlbfs nodev $HUGEPAGES_PATH
+fi
+HUGEPAGES_AVAIL=$(grep HugePages_Total /sys/devices/system/node/node0/meminfo | cut -d ':' -f 2|sed 's/ //g')
+if [ $HUGEPAGES_AVAIL -ne $HUGEPAGES_NUM ]; then 
+	printf "Warning: %s hugepages available, %s requested\n" "$HUGEPAGES_AVAIL" "$HUGEPAGES_NUM"
+fi
 
