c4e76c6925e1a122a1b9596447182e48c785ab19
cardigliano <cardigliano@ntop.org>
2018-05-09 00:14:53 +0200

    example of custom flow marking

--- userland/examples_ft/ftflow.c
+++ userland/examples_ft/ftflow.c
@@ -144,6 +144,9 @@ void my_sigalarm(int sig) {
 /* This callback is called when a packet has been classified
 void processFlowPacket(const u_char *data, pfring_ft_packet_metadata *metadata, pfring_ft_flow *flow, void *user) {
   printf("[Packet Classified]\n");
+
+  // Marking the flow to discard all packets (this can be used to implement custom filtering policies)
+  pfring_ft_flow_set_action(flow, PFRING_FT_ACTION_DISCARD);
 }
 */
 
