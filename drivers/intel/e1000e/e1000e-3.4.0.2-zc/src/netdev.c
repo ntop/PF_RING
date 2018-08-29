c7542957d901b9e891b77b8e09919e32ecf328b5
cardigliano <cardigliano@ntop.org>
2018-08-29 11:06:27 +0200

    wait for *_clean_rx_irq to complete on first queue only, increased wait time

--- drivers/intel/e1000e/e1000e-3.4.0.2-zc/src/netdev.c
+++ drivers/intel/e1000e/e1000e-3.4.0.2-zc/src/netdev.c
@@ -4480,8 +4480,12 @@ void notify_function_ptr(void *rx_data, void *tx_data, u_int8_t device_in_use) {
 	if (device_in_use) {
 		/* Free all memory */
 
-		if (atomic_inc_return(&adapter->pfring_zc.usage_counter) == 1 /* first user */)
+		if (atomic_inc_return(&adapter->pfring_zc.usage_counter) == 1 /* first user */) {
 			try_module_get(THIS_MODULE); /* ++ */
+
+			/* wait for e1000_clean_rx_irq to complete the current receive if any */
+			usleep_range(100, 200);
+		}
 	
 #ifdef ENABLE_RX_ZC
 		if (debug_notify) 
@@ -4491,10 +4495,6 @@ void notify_function_ptr(void *rx_data, void *tx_data, u_int8_t device_in_use) {
 
 		if ((rx_ring != NULL)
 		    && (atomic_inc_return(&rx_ring->pfring_zc.queue_in_use) == 1 /* first user */)) {
-
-			/* wait for e1000_clean_rx_irq to complete the current receive if any */
-			usleep_range(10, 20);
-
 			/* disable receives while setting up the descriptors */
 			disable_receives(adapter);
 			e1000_irq_disable(adapter);
--- drivers/intel/i40e/i40e-2.4.6-zc/src/i40e_main.c
+++ drivers/intel/i40e/i40e-2.4.6-zc/src/i40e_main.c
@@ -3760,15 +3760,17 @@ void notify_function_ptr(void *rx_data, void *tx_data, u_int8_t device_in_use)
 
 	if (device_in_use) { /* free all memory */
 
-		if ((n = atomic_inc_return(&adapter->pfring_zc.usage_counter)) == 1 /* first user */)
+		if ((n = atomic_inc_return(&adapter->pfring_zc.usage_counter)) == 1 /* first user */) {
 			try_module_get(THIS_MODULE); /* ++ */
+
+			/* wait for i40e_clean_rx_irq to complete the current receive if any */
+			usleep_range(100, 200);  
+		}
+
     
 		if (rx_ring != NULL && atomic_inc_return(&rx_ring->pfring_zc.queue_in_use) == 1 /* first user */) {
 			struct i40e_vsi *vsi = rx_ring->vsi;
 			u16 pf_q = vsi->base_queue + rx_ring->queue_index;
-
-			/* wait for i40e_clean_rx_irq to complete the current receive if any */
-			usleep_range(10, 20);  
 
 			if (unlikely(enable_debug))
 				printk("[PF_RING-ZC] %s:%d RX Tail=%u\n", __FUNCTION__, __LINE__, readl(rx_ring->tail));
