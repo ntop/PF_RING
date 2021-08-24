a63bd6ac5512d1ea3ee6269513dabef6390e40eb
Alfredo Cardigliano <cardigliano@ntop.org>
2021-08-24 17:31:31 +0200

    Print message on namespace lookup failure

--- kernel/pf_ring.c
+++ kernel/pf_ring.c
@@ -768,7 +768,12 @@ void term_lockless_list(lockless_list *l, u_int8_t free_memory)
 /* ********************************** */
 
 pf_ring_net *netns_lookup(struct net *net) {
-  return net_generic(net, pf_ring_net_id);
+  pf_ring_net *pf_net = net_generic(net, pf_ring_net_id);
+
+  if (pf_net == NULL)
+    printk("[PF_RING] Namespace lookup failure\n");
+
+  return pf_net;
 }
 
 /* ********************************** */
@@ -1661,7 +1666,7 @@ static int ring_proc_get_info(struct seq_file *m, void *data_not_used)
       seq_printf(m, "Cluster Fragment Discard : %u\n", num_cluster_discarded_fragments);
     }
   } else {
-    /* Detailed statistics about a PF_RING */
+    /* Detailed statistics about a socket */
     struct pf_ring_socket *pfr = (struct pf_ring_socket *)m->private;
 
     if(pfr) {
@@ -1676,11 +1681,13 @@ static int ring_proc_get_info(struct seq_file *m, void *data_not_used)
       } else {
         list_for_each_safe(ptr, tmp_ptr, &ring_aware_device_list) {
  	  pf_ring_device *dev_ptr = list_entry(ptr, pf_ring_device, device_list);
-          int32_t dev_index = ifindex_to_pf_index(netns_lookup(dev_net(dev_ptr->dev)),
-                                                  dev_ptr->dev->ifindex);
-	  if(dev_index >= 0 && test_bit(dev_index, pfr->pf_dev_mask)) {
-	    seq_printf(m, "%s%s", (num > 0) ? "," : "", dev_ptr->dev->name);
-	    num++;
+          pf_ring_net *pf_net = netns_lookup(dev_net(dev_ptr->dev));
+          if (pf_net != NULL /* safety check */) {
+            int32_t dev_index = ifindex_to_pf_index(pf_net, dev_ptr->dev->ifindex);
+	    if(dev_index >= 0 && test_bit(dev_index, pfr->pf_dev_mask)) {
+	      seq_printf(m, "%s%s", (num > 0) ? "," : "", dev_ptr->dev->name);
+	      num++;
+            }
           }
 	}
       }
