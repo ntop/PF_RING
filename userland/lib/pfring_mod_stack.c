ab47b839134006f021af784b41eda6e358b64947
Alfredo Cardigliano <cardigliano@ntop.org>
2021-10-27 19:02:12 +0200

    Handle interfaces tied to a different system interface name in the stack module (e.g. mlx)

--- userland/lib/pfring_mod_stack.c
+++ userland/lib/pfring_mod_stack.c
@@ -17,14 +17,71 @@
 
 /* **************************************************** */
 
+static int __system_name_lookup(char *name, char *system_name, int system_name_len) {
+  pfring_if_t *pfdevs, *pfdev;
+  int found = 0;
+
+  pfdevs = pfring_findalldevs();
+
+  if (!pfdevs)
+    return 0;
+
+  pfdev = pfdevs;
+  while (pfdev != NULL) {
+
+    if (pfdev->name && pfdev->system_name) {
+      char *name = pfdev->name;
+      char *mod_colon, *at;
+
+      mod_colon = strchr(name, ':');
+      if (mod_colon)
+        name = &mod_colon[1];
+
+      at = strchr(name, '@');
+      if (at)
+        at[0] = '\0';
+
+      if (strcmp(pfdev->name, name) == 0) {
+        snprintf(system_name, system_name_len, "%s", pfdev->system_name);
+        found = 1;
+        break;
+      }
+    }
+
+    pfdev = pfdev->next;
+  }
+
+  pfring_freealldevs(pfdevs);
+
+  return found;
+}
+
+/* **************************************************** */
+
 int pfring_mod_stack_open(pfring *ring) {
-  int rc;
   u_int32_t dummy = 0;
+  int rc;
 
   rc = pfring_mod_open(ring);
 
-  if (rc != 0)
+  if (rc != 0) {
+    char system_name[24];
+    char *device_name;
+
+    /* Lookup device bound to a system dev with a different name (e.g. mlx) */
+    if (__system_name_lookup(ring->device_name, system_name, sizeof(system_name))) {
+      device_name = strdup(system_name);
+      if (device_name) {
+        free(ring->device_name);
+        ring->device_name = device_name;
+        rc = pfring_mod_open(ring);
+      }
+    }
+  }
+
+  if (rc != 0) {
     return rc;
+  }
 
   rc = setsockopt(ring->fd, 0, SO_SET_STACK_INJECTION_MODE, &dummy, sizeof(dummy));
 
