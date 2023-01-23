fa1fd2357d3d0a30b7b42c26bce0754758ec8231
Alfredo Cardigliano <cardigliano@ntop.org>
2023-01-23 11:48:47 +0100

    ixgbe driver update for RH9.1

--- drivers/intel/ixgbe/ixgbe-5.5.3-zc/src/kcompat_rhel_defs.h
+++ drivers/intel/ixgbe/ixgbe-5.5.3-zc/src/kcompat_rhel_defs.h
@@ -128,27 +128,20 @@
 #endif /* 8.5 */
 
 /*****************************************************************************/
-#if (RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(8,6))
-#else /* >= 8.6 */
-#if (RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(9,0)) /* HAVE_PF_RING */
-#define HAVE_ETHTOOL_COALESCE_EXTACK
-#endif
-#endif /* 8.6 */
-
-/*****************************************************************************/
-#if (RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(8,7))
-#else /* >= 8.7 */
-#if (RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(9,0)) /* HAVE_PF_RING */
-#define HAVE_ETHTOOL_EXTENDED_RINGPARAMS
-#endif
-#endif /* 8.6 */
-
-/*****************************************************************************/
 #if (RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(9,0))
+#define HAVE_ETHTOOL_EXTENDED_RINGPARAMS
+#define HAVE_ETHTOOL_COALESCE_EXTACK
 #else /* >= 9.0 */
 #ifndef HAVE_PF_RING
 #define HAVE_XDP_BUFF_RXQ
 #endif
 #endif /* 9.0 */
+
+/*****************************************************************************/
+#if (RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(9,1))
+#else /* >= 9.1 */
+#define HAVE_ETHTOOL_COALESCE_EXTACK
+#define HAVE_ETHTOOL_EXTENDED_RINGPARAMS
+#endif /* 9.1 */
 
 #endif /* _KCOMPAT_RHEL_DEFS_H_ */
