de5f90336838866392086b0b58a968ea44009620
Alfredo Cardigliano <cardigliano@ntop.org>
2024-01-10 16:56:12 +0100

    Fix auxiliary compilation for i40e

--- drivers/intel/i40e/i40e-2.24.6-zc/src/i40e_client.h
+++ drivers/intel/i40e/i40e-2.24.6-zc/src/i40e_client.h
@@ -5,7 +5,11 @@
 #define _I40E_CLIENT_H_
 
 #ifdef USE_INTEL_AUX_BUS
+#ifdef HAVE_PF_RING
+#include "auxiliary_bus.h"
+#else
 #include "linux/auxiliary_bus.h"
+#endif
 #else
 #include <linux/auxiliary_bus.h>
 #endif /* USE_INTEL_AUX_BUS */
