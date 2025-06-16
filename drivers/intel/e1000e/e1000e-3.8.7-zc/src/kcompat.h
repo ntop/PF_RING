e26e5cee988fe459048d3e6d2393525565d261c2
Alfredo Cardigliano <cardigliano@ntop.org>
2025-06-16 12:59:51 +0200

    Disable PTP_1588_CLOCK on e1000e due to missing convert_art_to_tsc on RH 9.6

--- drivers/intel/e1000e/e1000e-3.8.7-zc/src/kcompat.h
+++ drivers/intel/e1000e/e1000e-3.8.7-zc/src/kcompat.h
@@ -7504,6 +7504,7 @@ static inline int pci_enable_pcie_error_reporting(struct pci_dev __always_unused
 /* HAVE_PF_RING */
 #if (RHEL_RELEASE_CODE && (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9,6)))
 #define HAVE_ETHTOOL_GET_TS_KERNEL
+#undef CONFIG_PTP_1588_CLOCK /* Due to missing convert_art_to_tsc */
 #endif
 
 #endif
