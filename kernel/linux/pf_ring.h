84bcf340bb578b30322d4c77b0910baa3d682167
Luca Deri <deri@ntop.org>
2025-10-10 12:29:02 +0200

    Fix compilation on old kernels

--- kernel/linux/pf_ring.h
+++ kernel/linux/pf_ring.h
@@ -1408,6 +1408,10 @@ struct pf_ring_socket {
 
 /* **************************************** */
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5,11,0)
+#define SINGLE_PROT_HOOK
+#endif
+
 typedef struct {
   u_int32_t magic;
 
