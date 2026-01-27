daddba816f02fa4df163323736534dcbd34f747c
Alfredo Cardigliano <cardigliano@ntop.org>
2026-01-27 10:38:36 +0100

    Remove old ice version

--- drivers/intel/ice/ice-1.12.7-zc/src/ice_peer_support.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (C) 2018-2023 Intel Corporation */
-
-#ifndef _ICE_PEER_SUPPORT_H_
-#define _ICE_PEER_SUPPORT_H_
-int ice_init_peer(struct ice_pf *pf);
-void ice_deinit_peer(struct ice_pf *pf);
-void ice_remove_peer(struct ice_pf *pf);
-#endif /* _ICE_PEER_SUPPORT_H_ */
